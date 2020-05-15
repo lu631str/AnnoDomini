a29b37c1241b7c48f9b86d10c8e57153f5cecf15
Sarah Tiefert <sarah.tiefert@htwg-konstanz.de>
2020-05-15 12:42:34 +0200

    Defined 3 extra Classes and TestClasses

---
+++ src/test/scala/CardSpec.scala
@@ -0,0 +1,14 @@
+class CardSpec extends {
+
+  /*
+  a card
+  has a text, which is a String describing a historical event
+  has a date, which refers to the date the corresponding text-event happened
+  can show the text, without displaying the date
+  can display the date (with the text?)
+    a Player is not allowed to look at the date of his cards
+    the date of a card can only be revealed through doubting
+
+   */
+
+}
---
+++ src/test/scala/FieldSpec.scala
@@ -0,0 +1,27 @@
+class FieldSpec {
+/*
+  the field
+  has laid down cards,
+     there is always at least on card layed down
+     the laid down cards have an order
+
+
+  TUI:
+
+   Player1:
+   Card1: "der erste PC kommt auf den Markt",  Card2: text,  Card3: text
+
+   Player2:
+   Card1: text,  Card2: text,  Card3: text
+
+   Field:
+     Card1: text,  Card: text, Card: text
+
+   Current Player: Player1
+   what do you wanna do?
+
+   ablegen(C1, position(C1, C2))
+
+
+ */
+}

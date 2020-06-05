d3a1646fa1ce1ae2a44ae84e6dd11061029f2b50
lukas <lu631str@htwg-kontsnaz.de>
2020-06-05 13:14:06 +0200

    updated TabledSpec to 12%

--- src/test/scala/model/TableSpec.scala
+++ src/test/scala/model/TableSpec.scala
@@ -6,14 +6,26 @@ class TabledSpec extends WordSpec with Matchers {
     "new" should {
       val table = new Table(List(Card("Test1", 1800)), List(Player("player 1", List(Card("Test2", 1802)))),List(Card("Test2", 1802)))
       "have a collection of ordered cards" in{
-      table.cards should be (List(Card("Test1", 1800)))
+        table.cards should be (List(Card("Test1", 1800)))
         table.cards.isInstanceOf[List[Card]] should be (true)
+        table.cards should not equal(Nil)
       }
       "have at least one player" in{
         table.players should be (List(Player("player 1", List(Card("Test2", 1802)))))
         table.players.isInstanceOf[List[Player]] should be (true)
         table.players should not equal(Nil)
       }
+      "have deck" in{
+        table.deck should be (List(Card("Test2", 1802)))
+        table.deck.isInstanceOf[List[Card]] should be (true)
+      }
+      "have a method to show cards on the 'field'" in {
+        table.showCards should be ("Feld:\n" +List(Card("Test1", 1800)))
+
+
+      }
+
+
       "have a method place, which places a players card on the table" in {
 
       }

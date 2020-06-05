c4accda4a541ee0b2fd496265618ce36b278b998
lukas <lu631str@htwg-kontsnaz.de>
2020-06-05 11:16:02 +0200

    updated PlayerSpec to 85%

--- src/main/scala/model/Player.scala
+++ src/main/scala/model/Player.scala
@@ -1,13 +1,30 @@
 package model
+import scala.util.control
+
 
 case class Player(name: String, hand: List[Card]) {
   override def toString:String = name + hand.mkString(", ")
-  def selectCard(x:Int) =
-    (hand(x-1), Player(name, hand.patch(x-1, Nil, 1)))
+
   def showHand = name + ":\n" + hand.mkString(", ") + "\n"
-  def addCard(c: Card) = Player(name, c :: hand)
+  def addCard(c: Card) = Player(name +":", c :: hand)
 
-  def addCard(c: List[Card]) = Player(name, c ::: hand)
+  def addCard(c: List[Card]) = Player(name + ":", c ::: hand)
 
-  def removeCard(idx: Int) =(hand(idx),Player(name,hand.patch(idx,Nil,1)))
+
+  def removeCard(idx: Int): (Card, Player) = (hand(idx), Player(name, hand.patch(idx, Nil, 1)))
+
+    /*if (idx <= hand.length) {
+      println("")
+    return (hand(idx), Player(name, hand.patch(idx, Nil, 1)))
+  } else{
+      println("Out of Index, removed first Card")
+      return (hand(idx), Player(name, hand))
+  }
+
+  }
+*/
+
+
+
+
 }
--- src/test/scala/model/PlayerSpec.scala
+++ src/test/scala/model/PlayerSpec.scala
@@ -10,32 +10,39 @@
 package model
 
 import org.scalatest._
+ //import scala.util.control.Breaks
 
 class PlayerSpec extends WordSpec with Matchers {
-  "A Player" when { "new" should {
-    val player = Player("Your Name", List(Card("Test", 1800)))
-    "have a name"  in {
-      player.name should be("Your Name")
-      player.name.isInstanceOf[String] should be(true)
+  "A Player" when {
+    "new" should {
+      val player = Player("Your Name", List(Card("Test", 1800)))
+
+      "have a name" in {
+        player.name should be("Your Name")
+        player.name.isInstanceOf[String] should be(true)
+      }
+      "have a hand" in {
+        player.hand.isInstanceOf[List[Card]] should be(true)
+      }
+      "add a Card" in {
+        player.addCard(Card("Test2", 1801)) should be (Player("Your Name:", List(Card("Test2", 1801), Card("Test", 1800))))
+        player.addCard(List(Card("Test2", 1801), Card("Test3", 1802))) should be  (Player("Your Name:", List(Card("Test2", 1801),Card("Test3", 1802), Card("Test", 1800))))
+      }
+
+        "remove a Card" in {
+          player.removeCard(0) should be((player.hand(0), Player("Your Name", player.hand.patch(0, Nil, 1))))
+          //player.removeCard(2) should be("Out of Index")
+        }
+
+        "have a nice String representation" in {
+          player.toString should be("Your Name" + player.hand.mkString(", "))
+        }
+        "show Hand" in {
+          player.showHand should be("Your Name:\n" + Card("Test", 1800) + "\n")
+
+        }
+
+      }
     }
-    "have a hand" in {
-      player.hand.isInstanceOf[List[Card]] should be(true)
+  }
 
-    }
-
-    "shows Hand" in {
-      player.showHand should be("Your Name:\n" + Card("Test", 1800) + "\n")
-
-    }
-
-
-    "have a nice String representation" in {
-      player.toString should be("Your Name" + player.hand.mkString(", "))
-    }
-    "shows Hand" in {
-      player.showHand should be("Your Name:\n" + Card("Test", 1800) + "\n")
-
-    }
-
-  }}
-}

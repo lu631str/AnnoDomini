eb3ee155f24d7eff516d3bd6a6618f1ec56edf99
Sarah <sarah.tiefert@htwg-konstanz.de>
2020-06-05 12:59:10 +0200

    changed tui

--- src/main/scala/AnnoDomini.scala
+++ src/main/scala/AnnoDomini.scala
@@ -9,8 +9,23 @@ import controller.Controller
 object AnnoDomini {
   val controller = new Controller(Table(Nil, Nil, Nil))
   val tui = new Tui(controller)
+  controller.notifyObservers
+  val scanner = new Scanner(System.in)
   def main(args: Array[String]): Unit = {
     println("Wilkommen bei AnnoDomini!!!")
+
+    var input :String = ""
+    println("Enter how many players want to play?")
+    val players = scanner.nextInt()
+    controller.createRandomTable(players)
+    tui.update
+    do {
+      input = scanner.nextLine()
+      tui.processInputLine(input)
+      tui.update
+
+    } while(input != "q")
+
 
   }
 }

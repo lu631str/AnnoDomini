68898a9d43ab902d86b89b7af841e7642580e205
lukas <lu631str@htwg-kontsnaz.de>
2020-06-19 08:51:10 +0200

    added CardStackSpec

---
+++ src/test/scala/model/CardStackSpec.scala
@@ -0,0 +1,16 @@
+package model
+import org.scalatest._
+
+
+class CardStackSpec extends WordSpec with Matchers {
+
+  "A CardStack" when {
+    "using" should {
+      val cardstack = CardStack.initialize
+      "a new cardstack should be declared in a Table" in {
+        val table = Table(Nil, Nil, CardStack.initialize)
+        table.deck.isInstanceOf[List[Card]] should be(true)
+      }
+    }
+  }
+}

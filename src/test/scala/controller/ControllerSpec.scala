47802c3ea92f4838759d5e025f01a9e7a5584a9e
lukas <lu631str@htwg-kontsnaz.de>
2020-07-16 15:39:15 +0200

    ControllerSpec update

--- src/test/scala/controller/ControllerSpec.scala
+++ src/test/scala/controller/ControllerSpec.scala
@@ -15,27 +15,72 @@ class ControllerSpec extends WordSpec with Matchers {
           List(Card("Test3", 1803))))
 
       "have method to generate a random Table" in {
-        controller.createRandomTable(1) should be ()
+        controller.createRandomTable(1) should be()
 
       }
       "have a string presentation" in {
-       controller.tableToString.isInstanceOf[String] should be (true)
+        controller.tableToString.isInstanceOf[String] should be(true)
       }
       "have a method to draw cards" in {
-        controller.draw(1)  should be (())
-        controller.draw should be (())
+        controller.draw(1) should be(())
+        controller.draw should be(())
       }
       "have a method to place a Card" in {
-        controller.placeCard(0,0) should be (())
+        controller.placeCard(0, 0) should be(())
       }
       "have a method to check the card order" in {
-        controller.checkCardOrder should be (controller.table.checkCardOrder)
+        controller.checkCardOrder should be(controller.table.checkCardOrder)
 
       }
       "have a method to get Card Text From Player" in {
         controller.getCardTextFromPlayer(0) should be(controller.table.getCardTextFromPlayer(0))
 
       }
-    }
+      "have a method to get Card Text From cards" in {
+        controller.getCardTextFromCards(0) should be(controller.table.getCardTextFromCards(0))
+
+      }
+      "have a method to  get Listleght FromCards" in {
+        controller.getListleghtFromCards should be(controller.table.getListleghtFromCards)
+
+
+      }
+      "have a method to  get Listleght From Player" in {
+        controller.getListleghtFromPlayer should be(controller.table.getListleghtFromPlayer)
+      }
+
+
+      "have a method to return cards" in {
+        controller.returnCards should be(controller.table.returnCards)
+      }
+
+      "have a method to return Table" in {
+        controller.returnTable should be(controller.table.returnTable)
+      }
+      "have a method to return Hand" in {
+        controller.returnHand should be(controller.table.returnHand)
+      }
+      "have a method to return Deck" in {
+        controller.returnDeck should be(controller.table.returnDeck)
+      }
+      "have a method to return controller" in {
+        controller.returnController should be(controller)
+      }
+      "have a method to return name" in {
+        controller.returnName should be(controller.table.returnName)
+      }
+
+      "have a method to check again the card order" in {
+        controller.setCheckCardOrder should be(controller.table.setCheckCardOrder)
+
+      }
+
+
+
+
+
+
+
+      }
   }
 }
---
+++ src/test/scala/util/UndoManagerSpec.scala
@@ -0,0 +1,29 @@
+//package util
+//
+//import controller.PlayerPlaceCommand
+//import controller.controllerBase.Controller
+//import org.scalatest._
+//import model.modelBaseImpl._
+//
+//
+//  class UndoManagerSpec extends WordSpec with Matchers {
+//    val table = new Table(Nil,Nil,Nil)
+//    val controller = new Controller(table)
+//    val command = new PlayerPlaceCommand(0,0,controller)
+//    "A Table" when {
+//      "new" should {
+//        val undoManager = new UndoManager
+//
+//
+//        "have a dostep" in {
+//
+//          undoManager.doStep(command) should be ()
+//
+//
+//        }
+//      }
+//
+//    }
+//  }
+
+
